22a69042ea75c2ba244c1ff37def8764e24cef5e
Emma Bindi <emma.bindi@outlook.com>
2021-06-16 20:15:36 +1000

    Skeleton lwc created, exposed boatSarch it is on the app page now

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+ export default class BoatSearch extends LightningElement {
+    isLoading = false;
+    
+    handleLoading() { }
+    
+    handleDoneLoading() { }
+    
+    // This custom event comes from the form
+    searchBoats(event) { }
+    
+    createNewBoat() { }
+  }
+  
