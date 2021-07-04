d18d1b725bc66350554e4d9cde41455444823ef5
Emma Bindi <emma.bindi@outlook.com>
2021-07-04 16:29:58 +1000

    Missing fields in get no wonder not seeing anything! So now up to adding the form which displays details. Nothing done for that yet. When selecting boat tile it is getting the selected Id and showing name for now

--- force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
+++ force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -8,13 +8,13 @@ import BOAT_ID_FIELD from '@salesforce/schema/Boat__c.Id';
 import BOAT_NAME_FIELD from '@salesforce/schema/Boat__c.Name';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
-import { wire, api } from 'lwc';
+import { wire, api, track } from 'lwc';
 import { APPLICATION_SCOPE, MessageContext, subscribe } from 'lightning/messageService';
 
 const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
 
 export default class BoatDetailTabs extends LightningElement {
-  @api boatId;
+  @track boatId;
   label = {
     labelDetails,
     labelReviews,
@@ -24,7 +24,7 @@ export default class BoatDetailTabs extends LightningElement {
   };
   subscription = null;   
 
-  @wire(getRecord, { recordId: '$boatId', BOAT_FIELDS})
+  @wire(getRecord, { recordId: '$boatId', fields: BOAT_FIELDS})
   wiredRecord;
 
   @wire(MessageContext) messageContext;
@@ -45,14 +45,20 @@ export default class BoatDetailTabs extends LightningElement {
         console.log('were already subscribed');
       return;
     }
-    this.subscription = subscribe(this.messageContext, BOATMC, (message) => {
-        this.boatId = message.recordId;
-    }, { scope: APPLICATION_SCOPE });
+    this.subscription = subscribe(
+        this.messageContext, 
+        BOATMC, 
+        (message) => {
+            this.boatId = message.recordId;
+            console.log('this.boatId ' + this.boatId);
+            console.log('message ' + JSON.stringify(message));
+        },
+        { scope: APPLICATION_SCOPE }
+    );
   }
 
   connectedCallback() { 
       this.subscribeMC();
-      console.log('subscribed. ' + this.boatId);
   }
   
   // Navigates to record page
