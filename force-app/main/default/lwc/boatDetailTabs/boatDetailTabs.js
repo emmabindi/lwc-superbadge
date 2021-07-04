3876425ad2824de5ced84d245fbc14c63ed8b575
Emma Bindi <emma.bindi@outlook.com>
2021-07-04 15:10:20 +1000

    Add subscription, wired record. Slowly follow comment instructions nothing working yet

--- force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
+++ force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -5,12 +5,15 @@ import labelFullDetails from '@salesforce/label/c.Full_Details';
 import labelPleaseSelectABoat from '@salesforce/label/c.Please_select_a_boat'; 
 import BOAT_ID_FIELD from '@salesforce/schema/Boat__c.Id';
 import BOAT_NAME_FIELD from '@salesforce/schema/Boat__c.Name';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import BoatMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import { wire, api } from 'lwc';
+import { APPLICATION_SCOPE, MessageContext, subscribe } from 'lightning/messageService';
 
 const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
 
 export default class BoatDetailTabs extends LightningElement {
-  boatId;
-  wiredRecord;
+  @api boatId;
   label = {
     labelDetails,
     labelReviews,
@@ -18,24 +21,32 @@ export default class BoatDetailTabs extends LightningElement {
     labelFullDetails,
     labelPleaseSelectABoat,
   };
+
+  @wire(getRecord, { recordId: '$boatId'. BOAT_FIELDS})
+  wiredRecord;
+
+  @wire(MessageContext) messageContext;
   
-  // Decide when to show or hide the icon
-  // returns 'utility:anchor' or null
-  get detailsTabIconName() { }
-  
-  // Utilize getFieldValue to extract the boat name from the record wire
-  get boatName() { }
-  
-  // Private
-  subscription = null;
-  
-  // Subscribe to the message channel
-  subscribeMC() {
-    // local boatId must receive the recordId from the message
+  get detailsTabIconName() { 
+      return wiredRecord ? 'utility:anchor' : null;
   }
   
-  // Calls subscribeMC()
-  connectedCallback() { }
+  get boatName() { 
+    return getFieldValue(this.wiredRecord.data, BOAT_NAME_FIELD);
+  }
+  
+  subscription = null;   
+  
+  subscribeMC() {
+    if (this.subscription || this.recordId) {
+      return;
+    }
+    this.subscription = subscribe(this.messageContext, BOATMC, (message) => this.boatId = message.recordId, { scope: APPLICATION_SCOPE });
+  }
+
+  connectedCallback() { 
+      this.subscribeMC();
+  }
   
   // Navigates to record page
   navigateToRecordViewPage() { }
